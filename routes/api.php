8f38ed9457d054b88d0056717d096b36e518113b
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-02-09 14:59:32 +0100

    Route api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Client
+Route::get('/client', 'ClientController@index');
+Route::get('/client/{find}', 'ClientController@show');
